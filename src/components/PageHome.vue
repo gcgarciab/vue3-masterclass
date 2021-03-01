ffcd12ddf3b667abbe6ebb50ad344112eb05aef2
Camilo García <camilo.garcia@nuvu.cc>
2021-02-28 21:41:27 -0500

    Create ThreadList and Clean up the Homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,60 +1,20 @@
 <template>
-  <div
-    v-for="thread in threads"
-    :key="thread.id"
-    class="col-large push-top"
-  >
-    <h1>{{ thread.title }}</h1>
-
-    <div class="post-list">
-      <div
-        v-for="postId in thread.posts"
-        :key="postId"
-        class="post"
-      >
-        <div class="user-info">
-          <a href="#" class="user-name">{{ userById( postById(postId).userId ).name }}</a>
-
-          <a href="#">
-            <img :src="userById( postById(postId).userId ).avatar" alt="" class="avatar-large">
-          </a>
-
-          <p class="desktop-only text-small"> posts</p>
-        </div>
-
-        <div class="post-content">
-          <div>
-            <p>{{ postById(postId).text }}</p>
-          </div>
-        </div>
-
-        <div class="post-date text-faded">
-          {{ postById(postId).publishedAt }}
-        </div>
-      </div>
-    </div>
-  </div>
+  <h1>Welcome to the Forum</h1>
+  <ThreadList :threads="threads"/>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import ThreadList from '@/components/ThreadList'
 
 export default {
-  data () {
-    return {
-      threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
-    }
+  components: {
+    ThreadList
   },
 
-  methods: {
-    postById (postId) {
-      return this.posts.find(p => p.id === postId)
-    },
-
-    userById (userId) {
-      return this.users.find(p => p.id === userId)
+  data () {
+    return {
+      threads: sourceData.threads
     }
   }
 }
