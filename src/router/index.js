f85e31cbce08014f1f09fa4781b763780602096b
Camilo García <camilo.garcia@nuvu.cc>
2021-04-12 07:24:40 -0500

    Editing Threads

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Category from '@/pages/Category'
 import Forum from '@/pages/Forum'
 import ThreadShow from '@/pages/ThreadShow'
 import ThreadCreate from '@/pages/ThreadCreate'
+import ThreadEdit from '@/pages/ThreadEdit'
 import NotFound from '@/pages/NotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
@@ -68,6 +69,12 @@ const routes = [
     path: '/forum/:forumId/thread/create',
     name: 'ThreadCreate',
     component: ThreadCreate,
+    props: true
+  },
+  {
+    path: '/thread/:id/edit',
+    name: 'ThreadEdit',
+    component: ThreadEdit,
     props: true
   },
   {
