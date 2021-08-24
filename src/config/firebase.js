70b03c3d414ac0a05a269db2b878c45eb2166285
Camilo García <gcgarciab@unal.edu.co>
2021-08-24 07:27:53 -0500

    Using .env for Different Environment Configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: 'AIzaSyAFM5Z1JNZqFJV14IyiCAyGHvhGPdIynjA',
-  authDomain: 'vue-school-forum-2907a.firebaseapp.com',
-  projectId: 'vue-school-forum-2907a',
-  storageBucket: 'vue-school-forum-2907a.appspot.com',
-  messagingSenderId: '529892541230',
-  appId: '1:529892541230:web:a84a65eb0b4ed8a14630bf'
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
 }
