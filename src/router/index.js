d428fa3ad47e65110360a5b7745c6d6e39aa7210
Camilo García <gcgarciab@unal.edu.co>
2021-11-17 10:24:22 -0500

    Hiding Guest Pages from Logged In Users

--- src/router/index.js
+++ src/router/index.js
@@ -88,12 +88,14 @@ const routes = [
   {
     path: '/register',
     name: 'Register',
-    component: Register
+    component: Register,
+    meta: { requiresGuest: true }
   },
   {
     path: '/login',
     name: 'SignIn',
-    component: SignIn
+    component: SignIn,
+    meta: { requiresGuest: true }
   },
   {
     path: '/logout',
@@ -128,6 +130,9 @@ router.beforeEach(async (to, from) => {
   console.log(`🚦 navigating to ${to.name} from ${from.name}`)
   store.dispatch('unsubscribeAllSnapshots')
   if (to.meta.requiresAuth && !store.state.authId) {
+    return { name: 'SignIn' }
+  }
+  if (to.meta.requiresGuest && store.state.authId) {
     return { name: 'Home' }
   }
 })
